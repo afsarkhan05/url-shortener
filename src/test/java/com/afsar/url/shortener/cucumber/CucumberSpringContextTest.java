7f56916f089e86902487832c7f3e789b1be1006e
afsarkhan05 <contactafsar@gmail.com>
2025-06-25 13:01:15 -0400

    Create CucumberSpringContextTest.java

---
+++ src/test/java/com/afsar/url/shortener/cucumber/CucumberSpringContextTest.java
@@ -0,0 +1,8 @@
+package com.afsar.url.shortener.cucumber;
+
+import io.cucumber.junit.platform.engine.Cucumber;
+
+@Cucumber // This annotation tells JUnit 5 to run Cucumber tests
+public class CucumberSpringContextTest {
+    // This class is empty, it just serves as the test runner for Cucumber
+}
